bd3de000a3ff0200ea16e918e898916b696c1af0
Raam <raam.chauhan@softwire.com>
2020-11-05 18:57:22 +0000

    Update server and add index.html

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const app = require('express')()
+const http = require('http').createServer(app)
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
+    res.sendFile(__dirname + '/index.html');
+});
 
-app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
-})
+http.listen(3000, () => {
+    console.log('listening on *:3000')
+});
