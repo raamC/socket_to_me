fac8654d383087a1e050de677368770af626d070
Raam <raam.chauhan@softwire.com>
2020-11-05 19:54:59 +0000

    Update sockets to send messages

--- index.js
+++ index.js
@@ -7,12 +7,12 @@ app.get('/', (req, res) => {
 });
 
 io.on('connection', (socket) => {
-    console.log('a user connected')
-    socket.on('disconnect', () => {
-        console.log('user disconnected')
+    socket.on('chat message', (msg) => {
+        console.log('message: ' + msg);
+        io.emit('chat message', msg);
     });
 });
 
-http.listen(3000, () => {
-    console.log('listening on *:3000')
+http.listen(4000, () => {
+    console.log('listening on *:4000')
 });
